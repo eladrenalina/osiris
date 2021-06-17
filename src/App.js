28106222cd539fcc2eaf589e4ac36326a481dff3
eladrenalina <39766850+eladrenalina@users.noreply.github.com>
2021-06-17 01:37:09 -0300

    ADD ItemListContainer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@
 import './App.css';
 import { NavBar } from './components/navbar/Navbar.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {ItemListContainer} from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
      <div>
     <NavBar/>
+    <ItemListContainer/>
+    
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+export function ItemListContainer ()
+{
+    return(
+    <div>
+        <h1>
+            HOLA CAROLA
+        </h1>
+    </div>
+    )
+}
+
