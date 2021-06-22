4a582480603cf1d4fde2ce3b20ae9a550ee9f925
eladrenalina <39766850+eladrenalina@users.noreply.github.com>
2021-06-22 01:21:03 -0300

    add img

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -3,9 +3,11 @@ import { ItemCount } from "../itemcount/ItemCount";
 export function Card(props) {
     return (
         <div className="card">
-            <img src="..." className="card-img" />
+            <div>
+                <img src="src\img\montgomery.jpg" className="card-img" />
+            </div>
             <div className="body">
-                <h4>TITLE CARD</h4>
+                <h4>Montgomery</h4>
                 <ItemCount stock="10" initial="1" />
                 <></>
                 <a href="#" className='btn btn-primary'>GO AHEAD</a>
