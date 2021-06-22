6c66fddba4da7999c1c2271af2e4e202daebf00a
eladrenalina <39766850+eladrenalina@users.noreply.github.com>
2021-06-21 23:58:44 -0300

    add count

---
+++ src/components/itemcount/ItemCount.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+
+export const ItemCount = (props) => {
+const [valor, setValor] = useState(props.initial);
+
+
+function onAdd{
+
+}
+function onSubstract{
+
+}
+return (
+    <div>
+        <button>-</button>
+        <input type="text"
+    </div>
+)
+}
