d7ac0d25be5649a2f19a698dd7ac1fd402e89602
eladrenalina <39766850+eladrenalina@users.noreply.github.com>
2021-07-07 20:26:55 -0300

    add route

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@
 import './App.css';
 import { NavBar } from './components/navbar/Navbar.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {ItemListContainer} from './components/ItemListContainer/ItemListContainer.js';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer.js';
 import { ItemDetailContainer } from './components/itemDetailContainer/ItemDetailContainer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="p-3 mb-2 bg-secondary text-white">
-      <NavBar />
-      <ItemListContainer />
-      <ItemDetailContainer/>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" >
+        <NavBar />
+          <ItemListContainer />
+        </Route>
+        {/* <div className="p-3 mb-2 bg-secondary text-white"> */}
+        <Route exact path="/detail" component={ItemDetailContainer} />
+
+        {/* </div> */}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
